f54aa50e073b94b172fad7bd6a7e06001f593151
ojamskie <plasabas20495@mkt.ceu.edu.ph>
2019-01-08 12:53:18 +0800

    Run

--- BasicElements.java
+++ BasicElements.java
@@ -1,9 +1,18 @@
+import javax.swing.Jframe;
+
 public class BasicElements{
 
 	public static void main(String[] args) {
+		Jframe myFrame = Jframe();
+
+		myFrame.setDefualtCloseOperation(myFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+		myFrame.setSize(900, 600);
+
 		System.out.println("basic java elements");
+
 	}
-	
+
 }
 
 
